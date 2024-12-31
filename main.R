40a29bb251c8312a622c809291cba215addf8a08
davidjhurst <david.hurst@iota2.com>
2024-12-31 16:22:34 +0000

    First release of Hello World!

---
+++ main.R
@@ -0,0 +1,15 @@
+# Element-wise AND
+"Start"
+
+v <- 1:10
+a <- c(1:24)
+a2 <- array(a, dim = c(4, 3, 2))
+m <- matrix(c(1,2,3,4), nrow = 2, ncol = 2)
+df <- data.frame (
+  col_1  =  c("a", "b", "c"),
+  col_2  =  c(1, 2, 3),
+  col_3  =  c(10, 20, 30)
+)
+f <- factor(c("Jazz", "Rock", "Classic", "Rock"))
+
+"End"
